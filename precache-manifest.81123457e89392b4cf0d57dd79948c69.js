da0fef66a47f5539f3e9ed33c786fcbc9f8707c1
Carrie <chanscom7@gmail.com>
2020-07-01 14:25:38 -0400

    Updates

---
+++ precache-manifest.81123457e89392b4cf0d57dd79948c69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5e098f5f2f6a8516c4457e096782b4",
+    "url": "/react-tests/index.html"
+  },
+  {
+    "revision": "36b32b00fd93e78a8ad8",
+    "url": "/react-tests/static/css/main.08b27dfa.chunk.css"
+  },
+  {
+    "revision": "82eb655f89f340b3ddb4",
+    "url": "/react-tests/static/js/2.ac6f2abd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tests/static/js/2.ac6f2abd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36b32b00fd93e78a8ad8",
+    "url": "/react-tests/static/js/main.a71e9ca6.chunk.js"
+  },
+  {
+    "revision": "349144f520aa5eea3bde",
+    "url": "/react-tests/static/js/runtime-main.428c3551.js"
+  },
+  {
+    "revision": "11572a3e9484b881f7e3754325937ba4",
+    "url": "/react-tests/static/media/pexels-photo-235985pixabay.11572a3e.jpeg"
+  }
+]);
